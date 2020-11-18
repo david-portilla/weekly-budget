43c04afd70365d25ba00aa01cbee13cd7d08a4a7
David Portilla <david_portilla@hotmail.com>
2020-11-18 10:19:47 -0500

    set state with the amount of money

--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Question = () => {
+
+  const [ amount, saveAmount ] = useState(0)
+
+  // Save the budget on the state
+  const setBudget = e => {
+    saveAmount(parseInt(e.target.value, 10))
+  }
+  // Add the budget to the list
+  const addBudget = e => {
+    e.preventDefault()
+    // Validate value
+
+    // Send value
+  }
+
   return (
     <>
       <h3>Add your $$$ budget for this week</h3>
-      <form>
+      <form
+        onSubmit={ addBudget }
+      >
         <input
           type="number"
           className="u-full-width"
           placeholder="Add an estimate of your budget"
+          onChange={ setBudget }
         />
 
         <input
