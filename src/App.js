5cc231094924e5de0bb5d0f0e4f593a9956a0509
David Portilla <david_portilla@hotmail.com>
2020-11-25 12:22:16 -0500

    adding components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import Form from './Form';
-import List from './List';
-import Question from './Question';
+import Form from './components/Form';
+import List from './components/List';
+import Question from './components/Question';
 
 function App () {
 
